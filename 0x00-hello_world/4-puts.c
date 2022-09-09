565ee209829dd5f84dc23d9c71fa5784d414f572
Biglizzie <awoniranelizabeth@gmail.com>
2022-09-08 20:51:07 -0700

    puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char my_str[] = "\"Programming is like building a multilingual puzzle";
+
+	puts(my_str);
+
+	return (0);
+}	
