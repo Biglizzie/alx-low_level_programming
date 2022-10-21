414b8c0fc6e13d11432751fecf01f01a4ff117f2
Biglizzie <awoniranelizabeth@gmail.com>
2022-10-21 05:29:18 -0700

    dog

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -2,54 +2,16 @@
 #include <stdlib.h>
 
 /**
- * new_dog - function that creates a new dog
+ * free_dog - function that frees dogs
  *
- * @name: pointer to the dog's name
- * @age: dog's age
- * @owner: pointer to the dog's owner
- * Return: pointer to the new dog
+ * @d: pointer to the dog array
  */
 
-dog_t *new_dog(char *name, float age, char *owner)
+void free_dog(dog_t *d)
 {
-	dog_t *newDog;
-	int nameLen = 0, ownerLen = 0, index;
-
-	while (name[nameLen] != '\0')
-	{
-	nameLen++;
-	}
-	while (owner[ownerLen] != '\0')
-	{
-		ownerLen++;
-	}
-	newDog = malloc(sizeof(dog_t));
-	if (newDog == NULL)
-	{
-		free(newDog);
-		return (NULL);
-	}
-	newDog->name = malloc(sizeof(newDog->name) * nameLen);
-	if (newDog->name == NULL)
-	{
-		free(newDog->name);
-		free(newDog);
-		return (NULL);
-	}
-	for (index = 0; index <= nameLen; index++)
-	{
-		newDog->name[index] = name[index];
-	}
-	newDog->age = age;
-	newDog->owner = malloc(sizeof(newDog->owner) * ownerLen);
-	if (newDog->owner == NULL)
-	{
-		free(newDog->owner);
-		free(newDog->name);
-		free(newDog);
-		return (NULL);
-	}
-	for (index = 0; index <= ownerLen; index++)
-		newDog->owner[index] = owner[index];
-	return (newDog);
+	if (d == NULL)
+		return;
+	free(d->name);
+	free(d->owner);
+	free(d);
 }
